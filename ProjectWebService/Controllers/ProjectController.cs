e2e3650478e9bba11a6c61a28b2bbf9a875df498
Nick Ward <nickward@microsoft.com>
2016-08-16 15:48:55 +1000

    Added stateless front end api to stateful service

---
+++ ProjectWebService/Controllers/ProjectController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ProjectService.Models;
+using ProjectStatefulService.Interfaces;
+using Microsoft.ServiceFabric.Services.Client;
+using Microsoft.ServiceFabric.Services.Remoting.Client;
+
+namespace ProjectWebService.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProjectController : Controller
+    {
+        // POST api/project
+        [HttpPost]
+        public async Task<Project> CreateProject(string projectName)
+        {
+            if(projectName == null)
+            {
+                HttpContext.Response.StatusCode = 400;
+                return null;
+            }
+
+            if(projectName.Trim() == string.Empty)
+            {
+                HttpContext.Response.StatusCode = 400;
+                return null;
+            }
+
+            IProject projectProxy = ServiceProxy.Create<IProject>(new Uri("fabric:/ProjectService/ProjectStatefulService"), new ServicePartitionKey(0));
+
+            try
+            {
+                Project newProject = await projectProxy.CreateEmptyProject(projectName);
+                return newProject;
+            }
+            catch (Exception e)
+            {
+                HttpContext.Response.StatusCode = 500;
+                return null;
+            }
+        }
+
+        // GET api/project
+        [HttpGet]
+        public async Task<Project> GetProject(string projectId)
+        {
+            Guid projectIdGuid;
+
+            if (!Guid.TryParse(projectId, out projectIdGuid))
+            {
+                HttpContext.Response.StatusCode = 400;
+                return null;
+            }
+
+            IProject projectProxy = ServiceProxy.Create<IProject>(new Uri("fabric:/ProjectService/ProjectStatefulService"), new ServicePartitionKey(0));
+
+            try
+            {
+                Project foundProject = await projectProxy.GetProjectById(projectIdGuid);
+                if(foundProject.Name == null)
+                {
+                    HttpContext.Response.StatusCode = 404;
+                    return null;
+                }
+                return foundProject;
+            }
+            catch (Exception e)
+            {
+                HttpContext.Response.StatusCode = 500;
+                return null;
+            }
+        }
+    }
+}
---
+++ ProjectWebService/Controllers/ValuesController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ProjectStatefulService.Interfaces;
+using ProjectService.Models;
+using Microsoft.ServiceFabric.Services.Remoting.Client;
+using Microsoft.ServiceFabric.Services.Client;
+
+namespace ProjectWebService.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public async Task<IEnumerable<string>> Get()
+        {
+            IProject projectProxy = ServiceProxy.Create<IProject>(new Uri("fabric:/ProjectService/ProjectStatefulService"), new ServicePartitionKey(0));
+
+            try
+            {
+                Project newProject = await projectProxy.CreateEmptyProject("Test Project");
+                return new string[] { newProject.Id.ToString(), newProject.Name, newProject.TaskIdList.Count.ToString(), newProject.Status };
+            } catch (Exception e)
+            {
+                return new string[] { "Error occurred"};
+            }            
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
