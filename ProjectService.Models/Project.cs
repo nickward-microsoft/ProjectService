18c70d17ae48a5f25cb37e57f2083d93959ccac7
Nick Ward <nickward@microsoft.com>
2016-08-16 14:26:22 +1000

    Intitial stateful ProjectStatefulService, with interface for communication

---
+++ ProjectService.Models/Project.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace ProjectService.Models
+{
+    public class Project
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public List<int> TaskIdList { get; set; }
+        public string Status { get; set; }
+    }
+}
---
+++ ProjectStatefulService.Interfaces/IProject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProjectService.Models;
+
+namespace ProjectStatefulService.Interfaces
+{
+    using Microsoft.ServiceFabric.Services.Remoting;
+
+    public interface IProject : IService
+    {
+        Task<Project> CreateEmptyProject(string projectName);
+        Task<Project> GetProjectById(Guid projectId);
+    }
+}
