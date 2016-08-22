b5a35749aaebf58f417a4f1444be1753a9f457df
nickward-microsoft <nickward@microsoft.com>
2016-08-22 11:13:38 +1000

    Moved Interfaces and Models into ProjectWebService project to remove dependencies that I couldn't figure out Changed build order to ensure that models and Interfaces get built first

---
+++ ProjectWebService/IProject.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProjectService.Models;
+
+namespace ProjectStatefulService.Interfaces
+{
+    using Microsoft.ServiceFabric.Services.Remoting;
+
+    public interface IProject : IService
+    {
+        Task<IEnumerable<Project>> GetProjects();
+        Task<Project> GetProjectByName(string projectName);
+        Task<Project> CreateEmptyProject(string projectName);
+        Task<Project> AddTaskToProject(string projectName, int taskId);
+        Task<Project> CompleteProject(string projectName);
+        Task DeleteProject(string projectName);
+    }
+}
---
+++ ProjectWebService/Project.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace ProjectService.Models
+{
+    public class Project
+    {
+        public string Name { get; set; }
+        public List<int> TaskIdList { get; set; }
+        public string Status { get; set; }
+    }
+}
